0b5fd1076fdd64acc5216a5fff9a89044809965e
nastik-squirrel <nastikk@gmail.com>
2020-07-07 23:25:41 +0300

    try2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
     document.querySelector("h2").innerText = new Date().toLocaleString().slice(0,17)
-    setInterval(() => document.querySelector("h2").innerText = new Date().toLocaleString(), 60000)
+    setInterval(() => document.querySelector("h2").innerText = new Date().toLocaleString(), 1000)
 
     // let dateTime = new Date().toLocaleString();
     // document.querySelector("h2").innerText = dateTime;
@@ -18,14 +18,24 @@
     //     item.value = "";
     //   },false)
   
+    function reverseChecked(target){
+      if(target.classList.contains('checked')){
+        target.classList.remove('checked');
+      } else {
+        target.classList.add('checked');
+      }
+    }
+
+    function modifyElement(target){
+      var newElement = document.createElement('li');
+      newElement.innerText = item.value;
+      list.appendChild(newElement);
+      store();
+      item.value = "";
+    }
 
     list.addEventListener('click',function(e){
-      var t = e.target;
-      if(t.classList.contains('checked')){
-        t.classList.remove('checked');
-      } else {
-        t.classList.add('checked');
-      }
+      reverseChecked(e.target)
       store();
     },false)
 
@@ -39,16 +49,43 @@
 
 {/* <span id="closePopupButton" type="button">X</span> */}
 
+    function createNewTodoItem (text) {
+      var todoItem = document.createElement('li');
+      todoItem.innerText = text;
+      // todoItem.className = "todo-task";
+      // var span = document.createElement('span');
+      // span.className = "task-title";
+      // span.innerText = text;
+      // var completed = document.createElement('a');
+      // completed.className = 'button toggle-complete';
+      // completed.href = "#";
+      // var info1 = document.createElement('i');
+      // info1.className = 'fas fa-check'
+      // completed.append(info1);
+      // var remove = document.createElement('a');
+      // remove.className = 'button remove';
+      // remove.href = "#";
+      // var info2 = document.createElement('i');
+      // info2.className = 'fas fa-trash'
+      // remove.append(info2);
+      // todoItem.appendChild(span);
+      // todoItem.appendChild(completed);
+      // todoItem.appendChild(remove);
+      return todoItem
+    }
+
     function addElement (e) {
         e.preventDefault();
-        var newElement = document.createElement('li');
+        list.appendChild(createNewTodoItem(item.value));
+        // var newElement = document.createElement('li');
         // var closeButton = document.createElement('span');
         // closeButton.classList.add("closeButton");
         // closeButton.nodeType = "button";
         // closeButton.innerText = "X";
         // newElement.append(closeButton);
-        newElement.innerText = item.value;
-        list.appendChild(newElement);
+        
+        // newElement.innerText = item.value;
+        // list.appendChild(newElement);
         // list.innerHTML += '<li>' + item.value + '</li>';
         store();
         item.value = "";
