67abe722e21157e3efa88631c32447f8d1722794
nastik-squirrel <46624815+nastik-squirrel@users.noreply.github.com>
2020-07-11 20:42:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 
     document.querySelector("h2").innerText = new Date().toLocaleString().slice(0,17)
-    setInterval(() => document.querySelector("h2").innerText = new Date().toLocaleString(), 1000)
+    setInterval(() => document.querySelector("h2").innerText = new Date().toLocaleString(), 60000)
 
     // let dateTime = new Date().toLocaleString();
     // document.querySelector("h2").innerText = dateTime;
 
-    var list = document.querySelector('#list'),
-        form = document.querySelector('form'),
+    let list = document.querySelector('#todo-list'),
+        form = document.querySelector('#form'),
         item = document.querySelector('#item');
     
-    form.addEventListener('submit', addElement, false)
+    form.addEventListener('submit', addItem, false);
  
     // form.addEventListener('submit',function(e){
     //     e.preventDefault();
@@ -18,77 +18,112 @@
     //     item.value = "";
     //   },false)
   
-    function reverseChecked(target){
-      if(target.classList.contains('checked')){
-        target.classList.remove('checked');
-      } else {
-        target.classList.add('checked');
-      }
-    }
 
-    function modifyElement(target){
-      var newElement = document.createElement('li');
-      newElement.innerText = item.value;
-      list.appendChild(newElement);
-      store();
-      item.value = "";
-    }
+    // list.addEventListener('click',function(e){
+    //   var t = e.target;
+    //   if(t.classList.contains('checked')){
+    //     t.classList.remove('checked');
+    //   } else {
+    //     t.classList.add('checked');
+    //   }
+    //   store();
+    // },false)
 
-    list.addEventListener('click',function(e){
-      reverseChecked(e.target)
-      store();
-    },false)
-
-    list.addEventListener('dblclick',function(e){
-        var t = e.target;
-        if(t.classList.contains('checked')){
-          t.parentNode.removeChild(t);
-        }
-        store();
-      },false)
+    // list.addEventListener('dblclick',function(e){
+    //     var t = e.target;
+    //     if(t.classList.contains('checked')){
+    //       t.parentNode.removeChild(t);
+    //     }
+    //     store();
+    //   },false)
 
 {/* <span id="closePopupButton" type="button">X</span> */}
 
-    function createNewTodoItem (text) {
-      var todoItem = document.createElement('li');
-      todoItem.innerText = text;
-      // todoItem.className = "todo-task";
-      // var span = document.createElement('span');
-      // span.className = "task-title";
-      // span.innerText = text;
-      // var completed = document.createElement('a');
-      // completed.className = 'button toggle-complete';
-      // completed.href = "#";
-      // var info1 = document.createElement('i');
-      // info1.className = 'fas fa-check'
-      // completed.append(info1);
-      // var remove = document.createElement('a');
-      // remove.className = 'button remove';
-      // remove.href = "#";
-      // var info2 = document.createElement('i');
-      // info2.className = 'fas fa-trash'
-      // remove.append(info2);
-      // todoItem.appendChild(span);
-      // todoItem.appendChild(completed);
-      // todoItem.appendChild(remove);
-      return todoItem
+    function removeItem (e) {
+      e.target.parentNode.remove()
     }
 
-    function addElement (e) {
-        e.preventDefault();
-        list.appendChild(createNewTodoItem(item.value));
-        // var newElement = document.createElement('li');
-        // var closeButton = document.createElement('span');
-        // closeButton.classList.add("closeButton");
-        // closeButton.nodeType = "button";
-        // closeButton.innerText = "X";
-        // newElement.append(closeButton);
-        
-        // newElement.innerText = item.value;
-        // list.appendChild(newElement);
-        // list.innerHTML += '<li>' + item.value + '</li>';
-        store();
-        item.value = "";
+    function toggleChecked (e) {
+      let parent = e.target.parentNode    
+      
+      if (parent.classList.contains('checked')) {
+        parent.classList.remove('checked');  
+        document.getElementById(`checkedButton-${parent.id}`).innerHTML = '&#9633';
+        document.getElementById(`a-${parent.id}`).classList.remove('checked');
+      } else {
+        parent.classList.add('checked');  
+        document.getElementById(`checkedButton-${parent.id}`).innerHTML = '&#10003';
+        document.getElementById(`a-${parent.id}`).classList.add('checked');
+      }
+    }
+
+    function changeItem (e) {
+      if (e.target.parentNode.classList.contains('todo-list')) {
+        targetId = e.target.id;
+      } else targetId = e.target.parentNode.id   
+      
+      let target = document.getElementById(`a-${targetId}`);
+
+      let answer = prompt('Change ToDo item:', target.innerHTML);
+      if (answer) {
+        target.innerHTML = answer
+      }
+    }
+
+    function addItem (e) {
+
+      e.preventDefault();
+
+      let itemId = new Date().getTime();
+      let colors = ['red', 'yellow', 'green', 'blue', 'gray']
+      
+      let newLi = document.createElement('li');
+      newLi.classList.add('todo-task');
+      // newLi.onclick = toggleChecked;
+      newLi.ondblclick = changeItem;
+      newLi.id = itemId;
+
+      let newSelect = document.createElement('select');
+      newSelect.classList.add('color-select');
+      newSelect.onchange = changeColor;
+      newSelect.id = `select-${itemId}`
+
+      for (let i in colors) {
+        let newOption = document.createElement('option');
+        newOption.classList.add(colors[i]);
+        newSelect.appendChild(newOption);
+      }
+
+      let newCheckedButton = document.createElement('span');
+      newCheckedButton.classList.add('checked-button');
+      newCheckedButton.type = 'button';
+      newCheckedButton.innerHTML = '&#9633';
+      newCheckedButton.onclick = toggleChecked;
+      newCheckedButton.id = `checkedButton-${itemId}`;
+
+      let newRemoveButton = document.createElement('span');
+      newRemoveButton.classList.add('remove-button');
+      newRemoveButton.type = 'button';
+      newRemoveButton.innerHTML = '&#128465';
+      newRemoveButton.onclick = removeItem;
+      newRemoveButton.id = `removeButton-${itemId}`;
+      
+      let newA = document.createElement('a');
+      newA.classList.add('item-label');
+      newA.innerText = item.value;
+      newA.onclick = toggleChecked;
+      newA.id = `a-${itemId}`;
+
+      newLi.appendChild(newSelect);
+      newLi.appendChild(newCheckedButton);
+      newLi.appendChild(newA);
+      newLi.appendChild(newRemoveButton);
+
+      list.appendChild(newLi);
+
+      item.value = "";
+      // store();
+
     }
 
     function store() {
@@ -107,4 +142,10 @@
         list.innerHTML = storedValues;
       }
     }
-    getValues();
+
+    function changeColor(e) {
+      e.target.classList.remove(...e.target.classList);
+      e.target.classList.add(e.target.options[e.target.selectedIndex].classList[0]);
+    }
+
+    // getValues();
